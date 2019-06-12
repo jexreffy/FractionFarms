55b37b11a6d10357cca2b08bdd03732cd07279f2
Jeffrey Bennett <jexreffy@icloud.com>
2019-06-12 11:45:47 -0500

    Ending Screen

---
+++ Assets/Jexreffy/Scripts/UI/EndingScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Jexreffy.LoL;
+using TMPro;
+
+namespace Jexreffy.FractionFarms {
+    public class EndingScreen : MonoBehaviour {
+
+        public TextMeshProUGUI ScoreLabel;
+        public TextMeshProUGUI ScoreValue;
+
+        public Animator FaderAnimator;
+
+        private const string SCORE = "score";
+        private const string FADE_IN_TRIGGER = "FadeIn";
+
+        void Awake() {
+            ScoreLabel.text = PlatformController.Instance.GetText(SCORE);
+            ScoreValue.text = PlatformController.Instance.Score.ToString();
+
+            FaderAnimator.SetTrigger(FADE_IN_TRIGGER);
+        }
+    }
+}
